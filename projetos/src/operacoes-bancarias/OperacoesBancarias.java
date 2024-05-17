3dc73e4a1d8736dc113656713863cc0b94cf27ac
danielcastilhonascimento <danielcastilho_@hotmail.com>
2024-05-17 18:02:01 -0300

    Ajustando para passar nos testes automatizados

--- projetos/src/operacoes-bancarias/OperacoesBancarias.java
+++ projetos/src/operacoes-bancarias/OperacoesBancarias.java
@@ -1,4 +1,3 @@
-
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -8,18 +7,8 @@ public class OperacoesBancarias {
             Locale.setDefault(Locale.US);
             Scanner scanner = new Scanner(System.in); 
             double saldo = 0;
-            boolean emOperacao = true;
-            
+            boolean emOperacao = true;       
 
-            System.out.println("          Banco S/A");
-            System.out.println("============================");
-            System.out.println("Opções : ");
-            System.out.println("1 - Depositar");
-            System.out.println("2 - Sacar");
-            System.out.println("3 - Consultar Saldo");
-            System.out.println("0 - Encerrar");
-            System.out.println("============================");
-                        
             while (emOperacao == true) { 
                 
                 int opcao = scanner.nextInt();
@@ -28,23 +17,23 @@ public class OperacoesBancarias {
 
                 switch (opcao) {
                     case 1:
-                        System.out.print("Valor a ser depositado : ");
                         valorDigitadoCliente = scanner.nextDouble();
                         saldo += valorDigitadoCliente;
+                        System.out.printf("Saldo atual: %.1f \n", saldo);
                         break;
                     
                     case 2:
-                        System.out.print("Valor a ser sacado : ");
-                        valorDigitadoCliente = scanner.nextDouble();
+                        valorDigitadoCliente = scanner.nextDouble();                       
                         if(valorDigitadoCliente < saldo ){
-                            saldo -= valorDigitadoCliente;}
-                            else{
-                                System.out.println("Saldo insuficiente");
+                            saldo -= valorDigitadoCliente;
+                            System.out.printf("Saldo atual: %.1f \n", saldo);
+                        }else{
+                                System.out.println("Saldo insuficiente.");
                             }
                         break;
                     
                     case 3:
-                        System.out.printf("Saldo atual : %.2f", saldo);
+                        System.out.printf("Saldo atual : %.1f \n", saldo);
                         break;
 
                     case 0:
@@ -58,7 +47,7 @@ public class OperacoesBancarias {
             
         }
         scanner.close();
-        System.out.println("Operação encerrada! Obrigado por utilizar nossos serviços");
+        System.out.println("Programa encerrado.");
     }
 }
 
