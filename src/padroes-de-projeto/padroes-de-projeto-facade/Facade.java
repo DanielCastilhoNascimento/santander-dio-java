c2756372c75cc08332ece4f699c8acc30407f292
danielcastilhonascimento <danielcastilho_@hotmail.com>
2024-07-16 20:15:10 -0300

    Incluido classes de exemplo do padrão de projeto facade

---
+++ src/padroes-de-projeto/padroes-de-projeto-facade/Facade.java
@@ -0,0 +1,12 @@
+public class Facade {
+    public void migrarCliente(String nome, String cep){
+        String cidade = CepApi.getInstancia().recuperarCidade(cep);
+        String estado = CepApi.getInstancia().recuperarEstado(cep);
+
+        CrmService.gravarCliente(nome, cep, cidade, estado);
+        System.out.println(nome);
+        System.out.println(cep);
+        System.out.println(cidade);
+        System.out.println(estado);        
+    }
+}
---
+++ src/padroes-de-projeto/padroes-de-projeto-facade/TestandoFacade.java
@@ -0,0 +1,7 @@
+public class TestandoFacade {
+public static void main(String[] args) {
+    
+    Facade facade = new Facade();
+    facade.migrarCliente("Daniel", "06000-000");
+}
+}
